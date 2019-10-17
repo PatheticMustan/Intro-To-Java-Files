bab07af827863e8f20c605876e87abbcdc60baa9
Mustan Pathetic <patheticmustan@gmail.com>
2019-10-17 09:04:18 -0400

    Lesson6

---
+++ Lessons/Lesson6.java
@@ -0,0 +1,14 @@
+/*
+ * File: Lesson6.java
+ * Author: Kevin W
+ * 
+ * This program actually just prints two lines.
+ */
+
+public class Lesson6 {
+    public static void main (String[] args) {
+        System.out.println ("Java is fun!");
+        
+        System.out.println("So this guy in the front of the room says…");
+    }
+}
