0f035038c6f86d7dd25c1bd30d5191d5aea394cb
Mustan Pathetic <patheticmustan@gmail.com>
2019-11-06 15:08:17 -0500

    Update methods

--- Projects/Methods/Methods.java
+++ Projects/Methods/Methods.java
@@ -23,115 +23,157 @@
  * 3. Methods (10 points): Each method has the proper signature and works as expected.
  */
 
+import java.util.Scanner;
+
 public class Methods {
-    public static void main(String[] args) {
-        System.out.println("Testing factorial!");
-        System.out.println(factorial(0)); // I googled it, and apparently factorial(0) is supposed to be 1???
-        System.out.println(factorial(5)); // 120
-        System.out.println(factorial(9)); // 362880
-        System.out.println(factorial(6)); // 720
-        System.out.println(factorial(3)); // 6
-        
-        System.out.println("\nTesting firstDigit!");
-        System.out.println(firstDigit(412));  // 4
-        System.out.println(firstDigit(1253)); // 1
-        System.out.println(firstDigit(9871)); // 9
-        System.out.println(firstDigit(1252)); // 1
-        System.out.println(firstDigit(9172)); // 9
-        
-        System.out.println("\nTesting allDigitsOdd!");
-        System.out.println(allDigitsOdd(173));   // true
-        System.out.println(allDigitsOdd(12398)); // false
-        System.out.println(allDigitsOdd(11197)); // true
-        System.out.println(allDigitsOdd(2019));  // false
-        System.out.println(allDigitsOdd(1979));  // true
-        
-        System.out.println("\nTesting allDigitsSame!");
-        System.out.println(allDigitsSame(66666));     // true
-        System.out.println(allDigitsSame(11191));     // false
-        System.out.println(allDigitsSame(1));         // true
-        System.out.println(allDigitsSame(981723));    // false
-        System.out.println(allDigitsSame(333333333)); // true 
-        
-        System.out.println("\nTesting hasMidpoint!");
-        System.out.println(hasMidpoint(6, 4, 8)); // true
-        System.out.println(hasMidpoint(2, 45, 92)); // false
-        System.out.println(hasMidpoint(3, 4, 5)); // true
-        System.out.println(hasMidpoint(6, 0, 7)); // false
-        System.out.println(hasMidpoint(17, 5, 11)); // true
-    }
-    
-    
-    
-    public static int factorial(int n) {
-        int result = 1;
-        // Loops through 2 -> N, and multiples result with I each time. 
-        for (int i=2; i<=n; i++) {
-            result *= i;
-        }
-        
-        return result;
-    }
-    
-    
-    
-    public static int firstDigit(int n) {
-        int result = n;
-        // This works because after the "/ 10", it's cut off to an int.
-        while (result >= 10) {
-            result /= 10;
-        }
-        return result;
-        /* I looked at StackOverflow, and I don't know if that counts as
-         * copying code, so I wrote something else.
-         * The StackOverflow code is commented out below.
-         * 
-         * return Integer.parseInt((n + "").substring(0, 1));
-         */
-    }
-    
-    
-    
-    public static boolean allDigitsOdd(int n) {
-        int result = n;
-        // Loop through every digit...
-        while (result >= 10) {
-            // If the current digit is even..
-            if ((result % 10) % 2 == 0) {
-                return false;
-            }
-            result /= 10;
-        }
-        
-        // If we've looped through all the digits and haven't found an even digit, all the digits are odd.
-        return true;
-    }
-    
-    
-    
-    public static boolean allDigitsSame(int n) {
-        int result = n;
-        // Every single digit should be equal to the first digit.
-        int lastDigit = result % 10;
-        
-        while (result >= 10) {
-            if (result % 10 != lastDigit) {
-                return false;
-            }
-            result /= 10;
-        }
-        return true;
-    }
-    
-    
-    
-    public static boolean hasMidpoint(int a, int b, int c) {
-        // (a + b)/2 == c    can be re-written as    a + b == c + c
-        // Doing this makes sure you don't need to worry about floats, which is pretty cool I guess.
-        if (a + b == c + c) return true;
-        if (b + c == a + a) return true;
-        if (c + a == b + b) return true;
-        
-        return false;
-    }
+	public static void main(String[] args) {
+		System.out.println("Testing factorial!");
+		System.out.println(factorial(0)); // I googled it, and apparently factorial(0) is supposed to be 1???
+		System.out.println(factorial(5)); // 120
+		System.out.println(factorial(9)); // 362880
+		System.out.println(factorial(6)); // 720
+		System.out.println(factorial(3)); // 6
+		
+		System.out.println("\nTesting firstDigit!");
+		System.out.println(firstDigit(412));  // 4
+		System.out.println(firstDigit(1253)); // 1
+		System.out.println(firstDigit(9871)); // 9
+		System.out.println(firstDigit(1252)); // 1
+		System.out.println(firstDigit(9172)); // 9
+		
+		System.out.println("\nTesting allDigitsOdd!");
+		System.out.println(allDigitsOdd(173));   // true
+		System.out.println(allDigitsOdd(1781));  // false
+		System.out.println(allDigitsOdd(11197)); // true
+		System.out.println(allDigitsOdd(2019));  // false
+		System.out.println(allDigitsOdd(1979));  // true
+		
+		System.out.println("\nTesting allDigitsSame!");
+		System.out.println(allDigitsSame(66666));     // true
+		System.out.println(allDigitsSame(11191));     // false
+		System.out.println(allDigitsSame(1));         // true
+		System.out.println(allDigitsSame(981723));    // false
+		System.out.println(allDigitsSame(333333333)); // true 
+		
+		System.out.println("\nTesting hasMidpoint!");
+		System.out.println(hasMidpoint(6, 4, 8));   // true
+		System.out.println(hasMidpoint(2, 45, 92)); // false
+		System.out.println(hasMidpoint(3, 4, 5));   // true
+		System.out.println(hasMidpoint(6, 0, 7));   // false
+		System.out.println(hasMidpoint(17, 5, 11)); // true
+		
+		
+		
+		// User can decide what to test as well.
+		Scanner sc = new Scanner(System.in); // I keep forgetting to put in "System.in"
+		System.out.println("\n\n\nPick a method. (1-5)\n1. Factorial\n2. firstDigit\n3. allDigitsOdd\n4. allDigitsSame\n5. hasMidpoint");
+		int input = sc.nextInt();
+		int a, b, c; // Initializes variables so we can use them below without writing "int" 5 times
+		if (input == 1) {
+			System.out.println("Factorial: Input an integer.");
+			a = sc.nextInt();
+			System.out.println(factorial(a));
+		} else if (input == 2) {
+			System.out.println("FirstDigit: Input an integer.");
+			a = sc.nextInt();
+			System.out.println(firstDigit(a));
+		} else if (input == 3) {
+			System.out.println("AllDigitsOdd: Input an integer.");
+			a = sc.nextInt();
+			System.out.println(allDigitsOdd(a));
+		} else if (input == 4) {
+			System.out.println("AllDigitsSame: Input an integer.");
+			a = sc.nextInt();
+			System.out.println(allDigitsSame(a));
+		} else if (input == 5) {
+			System.out.println("HasMidpoint: Input an integer.");
+			a = sc.nextInt();
+			System.out.println("HasMidpoint: Input a second integer.");
+			b = sc.nextInt();
+			System.out.println("HasMidpoint: Input a third integer.");
+			c = sc.nextInt();
+			System.out.println(hasMidpoint(a, b, c));
+		} else {
+			System.out.println("Oops! Please input a number from 1-5.");
+		}
+	}
+	
+	
+	
+	public static int factorial(int n) {
+		int result = 1;
+		// Loops through 2 -> N, and multiples result with I each time. 
+		for (int i=2; i<=n; i++)
+			result *= i;
+		
+		return result;
+	}
+	
+	
+	
+	public static int firstDigit(int n) {
+		int result = n;
+		// This works because after the "/ 10" changes numbers
+		while (result >= 10)
+			result /= 10;
+		
+		return result;
+		// I looked at StackOverflow, and I don't know if that counts as
+		// copying code, so I wrote my own code, out of fear of the plagarism hammer.
+	}
+	
+	
+	
+	public static boolean allDigitsOdd(int n) {
+		int result = n;
+		// Loop through every digit...
+		while (result > 0) {
+			// If the current digit is even..
+			if ((result % 10) % 2 == 0)
+				return false;
+			result /= 10;
+		}
+		
+		// If we've looped through all the digits and haven't found an even digit, all the digits are odd.
+		return true;
+	}
+	
+	
+	
+	public static boolean allDigitsSame(int n) {
+		int result = n;
+		// Every single digit should be equal to the last digit.
+		int lastDigit = result % 10;
+		
+		while (result >= 10) {
+			if (result % 10 != lastDigit)
+				return false;
+			result /= 10;
+		}
+		return true;
+	}
+	
+	
+	
+	public static boolean hasMidpoint(int a, int b, int c) {
+		// (a + b)/2 == c    can be re-written as    a + b == c + c
+		// Doing this makes sure you don't need to worry about floats, which is pretty cool I guess.
+		if (a + b == c + c) return true;
+		if (b + c == a + a) return true;
+		if (c + a == b + b) return true;
+		
+		return false;
+	}
 }
+
+/* Sometimes, I use "let" to declare my variables, and get confused when it doesn't work.
+ * ____________________________________
+ *         *_****   
+ *       **  \ __ **   ________
+ *      **  O  O  **  |Hmm...|
+ *      *     >    *  /------
+ *      *   ____   * /
+ *      **     // ** 
+ *        *\\_//**   
+ *
+ */
