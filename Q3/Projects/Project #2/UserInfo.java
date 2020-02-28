2538ccbb2a02af66289e46e7d567348e7f85cc56
PatheticMustan <patheticmustan@gmail.com>
2020-02-27 22:36:50 -0500

    Project 2 final

--- Q3/Projects/Project #2/UserInfo.java
+++ Q3/Projects/Project #2/UserInfo.java
@@ -1,12 +1,44 @@
+/* UserInfo.java            by Kevin Wang
+ * 
+ * Create a class called UserInfo that will enable a user to register an account within a database. The registration must include the
+ * following information:
+ *     * Name
+ *     * Address
+ * Once registration is complete, you will then prompt the user for a username and password. Following this, you will then encrypt the
+ * user's password. You will store the registration information in a class named UserInfo, with appropriate instance variables,
+ * accessor/mutator methods, and a toString() method. You will also incorporate methods to handle the encryption of the password,
+ * as indicated below:
+ *     * You must provide an encrypt(String) method, which correctly encrypts its String parameter and returns the
+ *       corresponding encrypted String. The method must have the following signature:
+ *             public String encrypt(String s)
+ *     * Your driver class MUST invoke the encrypt method.
+ *     * You must provide the following methods for each encryption step:
+ *             o removeWhitespaceAndConvertToUpper
+ *             o substitute
+ *             o swapHalfsForEncrypt
+ *             o swapFirst2WithLast2
+ *             o swapMiddleChars
+ * Each of the above methods MUST have a String parameter and return a String.
+ * At the end of your program, you must display the information for the user, INCLUDING the encrypted password.
+ * 
+ * NOTE: You may NOT store the unencrypted password in your standalone class. You may store the encrypted version. You will
+ * be penalized if you store the unencrypted password in your standalone.
+ */
+
 import java.lang.Math; 
 
 public class UserInfo {
     private String name;
     private String address;
+    private String username; // Pathetic, PATHETIC MUSTAN HAAAA HAAAA HAAAA HAAAA
+    private String password; // Password should be encrypted in the user driver...
     
-    public UserInfo(String name, String address) {
-        this.name = name;
-        this.address = address;
+    public UserInfo() {
+        // Set defaults OwO UwU
+        this.name = "";
+        this.address = "";
+        this.username = "";
+        this.password = "";
     }
     
     // GS Name
@@ -17,17 +49,37 @@ public class UserInfo {
     public String getAddress() {return address;}
     public void setAddress(String address) {this.address = address;}
     
+    // GS Username
+    public String getUsername() {return username;}
+    public void setUsername(String username) {this.username = username;}
+    
+    // GS Password
+    public String getPassword() {return password;}
+    public void setPassword(String password) {this.password = password;}
+    
 
     
     
-    // Encrypt stuff
+    // Encrypt the password. It's better than just chaining all the functions together, okay?
+    // encrypt takes a string, and chains the five functions below to "encrypt" the password.
     public String encrypt(String s) {
-        return swapMiddleChars(swapFirst2WithLast2(swapHalfsForEncrypt(substitute(removeWhitespaceAndConvertToUpper(s)))));
+        s = removeWhitespaceAndConvertToUpper(s);
+        s = substitute(s);
+        s = swapHalfsForEncrypt(s);
+        s = swapFirst2WithLast2(s);
+        s = swapMiddleChars(s);
+        
+        return s;
     }
     
+    // Pretty simple, not sure why this is even worth any points.
+    // As the name suggest, removeWhitespaceAndConvertToUpper takes a string, removes whitespace, and converts it to uppercase.
     public String removeWhitespaceAndConvertToUpper(String s) {
         return s.trim().toUpperCase();
     }
+    
+    // Somebody had to manually enter all these values. Poor fellow....
+    // Subsitute takes a string and subsitutes certain characters with specific values.
     public String substitute(String s) {
         return s
             .replaceAll("A", "@")
@@ -37,33 +89,42 @@ public class UserInfo {
             .replaceAll("O", "*")
             .replaceAll("P", "#")
             .replaceAll("R", "&")
-            .replaceAll("S", "\\$")
+            .replaceAll("S", "\\$") // The "$" messes up the regex, so we have to escape it before we put it in #replaceAll.
             .replaceAll("T", "+")
             .replaceAll("V", "^")
             .replaceAll("X", "%")
             .replaceAll(" ", "_");
     }
     
+    // swapHalfs would have been a better name, we have no consistency I swear
+    // swapHalfs takes a string, splits it in half, and flips the half.
     public String swapHalfsForEncrypt(String s) {
         int len = s.length();
-        int half = (int)Math.ceil(len / 2);
+        int half = (int)Math.ceil((float)len / 2);
+        
         return
             s.substring(half, len) +
             s.substring(0, half);
     }
     
+    // I mostly did my work in Notability. Oh yeah yeah.
+    // swapFirst2WithLast2 takes a string, and swaps the first two characters with the last two characters.
     public String swapFirst2WithLast2(String s) {
         int len = s.length();
-        int half = (int)Math.ceil(len / 2);
+        int half = (int)Math.ceil((float)len / 2);
+        
         return
             s.substring(len - 2, len) +
             s.substring(2, len - 2) +
             s.substring(0, 2);
     }
     
+    // bruh everything is just more grinding, swapping characters
+    // swapMiddleChars takes a string, and swaps the two characters to the left of the middle, and the two characters to the right of the middle.
     public String swapMiddleChars(String s) {
         int len = s.length();
-        int half = (int)Math.ceil(len / 2);
+        int half = (int)Math.ceil((float)len / 2);
+        
         return
             s.substring(0, half - 2) +
             s.substring(half, half + 2) +
@@ -74,9 +135,14 @@ public class UserInfo {
     
     
     // toString
+    // toString returns all the relevant information about the UserInfo class.
     public String toString() {
         return
-            "Name:    " + name + "\n" +
-            "Address: " + address + "\n";
+            // I wish everything was in a monospaced font, the reason for the weird spacing is because this makes
+            // the text look aligned in JOptionPane
+            "Name:                             " + name     + "\n" +
+            "Address:                        " + address  + "\n" + 
+            "Username:                    " + username + "\n" + 
+            "Encrypted Password: " + password;
     }
 }
--- Q3/Projects/Project #2/UserInfoDriver.java
+++ Q3/Projects/Project #2/UserInfoDriver.java
@@ -1,35 +1,155 @@
+/* UserInfoDriver.java            by Kevin Wang
+ * UserInfoDriver tests the methods in UserInfo, and also takes user input, and user controlled exit.
+ * 
+ * Create a class called UserInfo that will enable a user to register an account within a database. The registration must include the
+ * following information:
+ *     ? Name
+ *     ? Address
+ * Once registration is complete, you will then prompt the user for a username and password. Following this, you will then encrypt the
+ * user's password. You will store the registration information in a class named UserInfo, with appropriate instance variables,
+ * accessor/mutator methods, and a toString() method. You will also incorporate methods to handle the encryption of the password,
+ * as indicated below:
+ *     ? You must provide an encrypt(String) method, which correctly encrypts its String parameter and returns the
+ *        corresponding encrypted String. The method must have the following signature:
+ *            public String encrypt(String s)
+ *     ? Your driver class MUST invoke the encrypt method.
+ *     ? You must provide the following methods for each encryption step:
+ *         o removeWhitespaceAndConvertToUpper
+ *         o substitute
+ *         o swapHalfsForEncrypt
+ *         o swapFirst2WithLast2
+ *         o swapMiddleChars
+ * Each of the above methods MUST have a String parameter and return a String.
+ * At the end of your program, you must display the information for the user, INCLUDING the encrypted password.
+ * NOTE: You may NOT store the unencrypted password in your standalone class. You may store the encrypted version. You will
+ * be penalized if you store the unencrypted password in your standalone.
+ */
+
+
+// import for JOptionPane
+import javax.swing.*;
+
 public class UserInfoDriver {
     public static void main(String[] args) {
-        UserInfo kevin = new UserInfo("William", "Antarctica");
+        // Now you can test my methods as well!
+        // This time Karen is the manager. Who's she gonna call, her boss?
+        JFrame karen = new JFrame();
+        UserInfo ourBelovedUser = new UserInfo();
+            
+        do {
+            ourBelovedUser.setName(JOptionPane.showInputDialog(karen, "Enter a new name"));
+            ourBelovedUser.setAddress(JOptionPane.showInputDialog(karen, "Enter a new address"));
+            ourBelovedUser.setUsername(JOptionPane.showInputDialog(karen, "Account Created!\nEnter a new username"));
+            ourBelovedUser.setPassword(ourBelovedUser.encrypt(JOptionPane.showInputDialog(karen, "Enter a new password")));
         
-        System.out.println(kevin.getName());
-        System.out.println(kevin.getAddress());
+            // test toString()
+            JOptionPane.showMessageDialog(karen, ourBelovedUser);
+        } while (JOptionPane.showConfirmDialog(karen, "Do you wish to exit?", "Exit", JOptionPane.YES_NO_OPTION) == 1);
+        // yes    = 0
+        // no     = 1
+        // cancel = 2
+        
+        
+        
+        
+        System.out.println("\n\n"); 
+        
+        
+        
+        
+        
+        
+        
+        // MY OWN TESTING METHODS
+        // Testing if get/set methods work properly.
+        // Putting in the wrong name
+        UserInfo kevin = new UserInfo();
+        kevin.setName("William"); // William
+        kevin.setAddress("Antarctica"); // BRRR very cold
+        kevin.setUsername("BadDiscordChatter"); // oh no I'm bad at discord lmao
+        kevin.setPassword("password1"); // bad password
+        
+        System.out.println(kevin.getName()); // William
+        System.out.println(kevin.getAddress()); // Antarctica
+        System.out.println(kevin.getUsername()); // Antarctica
+        System.out.println(kevin.getPassword()); // Antarctica
         
         System.out.println("\n\n");
         
         // Oh no, I put in the wrong name! Let me change it!
-        kevin.setName("Kevin");
-        kevin.setAddress("69 Nassau Drive, 11021 Great Neck NY US");
+        kevin.setName("Kevin"); // Setting the name to the correct name
+        kevin.setAddress("69 Nassau Drive"); // ah yes 69 funny funny
+        kevin.setUsername("Natsumi");
+        kevin.setPassword("correct horse battery staple"); // https://xkcd.com/936/
         
-        System.out.println(kevin.getName());
-        System.out.println(kevin.getAddress());
-        
+        System.out.println(kevin.getName()); // Kevin
+        System.out.println(kevin.getAddress()); // 69 Nassau Drive
+        System.out.println(kevin.getUsername()); // Natsumi
+        System.out.println(kevin.getPassword()); // correct horse battery staple
         System.out.println("\n\n");
         
-        String password = "   abcdefghijklmnopqrstuvwxyz   ";
         
-        // Testing
-        System.out.println("Original:                          " + password);
+        // PASSWORD TESTING
+        
+        
+        // RemoveWhitespaceAndConvertToUpper
+        // password with trailing whitespace.
+        String password = "   abcdefghijkLMNopqrStuvwXyz   ";
+        System.out.println("RemoveWhitespaceAndConvertToUpper: " + password);
         System.out.println("RemoveWhitespaceAndConvertToUpper: " + kevin.removeWhitespaceAndConvertToUpper(password));
-        // Subsitute needs uppercase chars.
-        System.out.println("Substitute:                        " + kevin.substitute(kevin.removeWhitespaceAndConvertToUpper(password)));
-        System.out.println("SwapHalfsForEncrypt:               " + kevin.swapHalfsForEncrypt(kevin.substitute(kevin.removeWhitespaceAndConvertToUpper(password))));
-        System.out.println("SwapFirst2WithLast2:               " + kevin.swapFirst2WithLast2(kevin.swapHalfsForEncrypt(kevin.substitute(kevin.removeWhitespaceAndConvertToUpper(password)))));
-        System.out.println("SwapMiddleChars:                   " + kevin.swapMiddleChars(kevin.swapFirst2WithLast2(kevin.swapHalfsForEncrypt(kevin.substitute(kevin.removeWhitespaceAndConvertToUpper(password))))));
+        
+        // Subsitute
+        password = "A B C D E F G H I J K L M N O P Q R S T U V W X Y Z";
+        System.out.println("Substitute:                        " + password);
+        System.out.println("Substitute:                        " + kevin.substitute(password));
+        
+        System.out.println("");
+        
+        // SwapHalfsForEncrypt even
+        password = "0123456789";
+        System.out.println("SwapHalfsForEncrypt:               " + password);
+        System.out.println("SwapHalfsForEncrypt:               " + kevin.swapHalfsForEncrypt(password));
+        
+        System.out.println("");
+        
+        // SwapHalfsForEncrypt odd
+        password = "123456789";
+        System.out.println("SwapHalfsForEncrypt:               " + password);
+        System.out.println("SwapHalfsForEncrypt:               " + kevin.swapHalfsForEncrypt(password));
+        
+        System.out.println("");
+        
+        // SwapFirst2WithLast2 even
+        password = "0123456789";
+        System.out.println("SwapFirst2WithLast2:               " + password);
+        System.out.println("SwapFirst2WithLast2:               " + kevin.swapFirst2WithLast2(password));
+        
+        System.out.println("");
+        
+        // SwapFirst2WithLast2 odd
+        password = "123456789";
+        System.out.println("SwapFirst2WithLast2:               " + password);
+        System.out.println("SwapFirst2WithLast2:               " + kevin.swapFirst2WithLast2(password));
+        
+        System.out.println("");
+        
+        // SwapMiddleChars even
+        password = "0123456789";
+        System.out.println("SwapMiddleChars:                   " + password);
+        System.out.println("SwapMiddleChars:                   " + kevin.swapMiddleChars(password));
+        
+        System.out.println("");
+        
+        // SwapMiddleChars odd
+        password = "123456789";
+        System.out.println("SwapMiddleChars:                   " + password);
+        System.out.println("SwapMiddleChars:                   " + kevin.swapMiddleChars(password));
+        
+        System.out.println("");
+        
+        // Encrypt
+        password = "   youre G0nnA h7v3 a b7d t1me   ";
+        System.out.println("Encrypt:                           " + password);
         System.out.println("Encrypt:                           " + kevin.encrypt(password));
-        
-        System.out.println("\n\n");
-        
-        System.out.println(kevin);
     }
 }
