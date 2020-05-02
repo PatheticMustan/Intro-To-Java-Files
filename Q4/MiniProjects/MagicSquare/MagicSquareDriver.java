696b6820777bff67bb3e1a1d8c3aaca1103c664c
PatheticMustan <patheticmustan@gmail.com>
2020-05-01 22:29:48 -0400

    input checking

--- Q4/MiniProjects/MagicSquare/MagicSquareDriver.java
+++ Q4/MiniProjects/MagicSquare/MagicSquareDriver.java
@@ -9,6 +9,8 @@
  * please also display the matrix that the user entered.
  */
 
+import javax.swing.*;
+
 // I wonder if there's a magic circle driver?
 // what car does the magic square driver drive in? Where does it drive to? WHO IS IT DRIVING? That's what we really need to know.
 public class MagicSquareDriver {
@@ -19,16 +21,31 @@ public class MagicSquareDriver {
         // I'm trying to think of a clever name, but all I can think of is Shizune Hakamichi, so there she is
         // she'll read your inputs cuz she can't hear 'em haha
         JFrame shizune = new JFrame();
-        SquareMatrix matrix = new SquareMatrix();
             
         do {
-            SquareMatrix matrix = JOptionPane.showInputDialog(shizune, "Enter a new name"));
-            ourBelovedUser.setAddress(JOptionPane.showInputDialog(shizune, "Enter a new address"));
-            ourBelovedUser.setUsername(JOptionPane.showInputDialog(shizune, "Account Created!\nEnter a new username"));
-            ourBelovedUser.setPassword(ourBelovedUser.encrypt(JOptionPane.showInputDialog(shizune, "Enter a new password")));
+            // I REALLY don't want to check for valid input but I also really don't want to lose points
+            int size = 0;
+            do {
+                size = Integer.parseInt(JOptionPane.showInputDialog(shizune, "Enter the size of the matrix."));
+                if (size < 1) {JOptionPane.showMessageDialog(shizune, "Invalid size! Enter a number greater than 0.");}
+            } while(size < 1);
+            
+            SquareMatrix matrix = new SquareMatrix(size);
+            
+            for (int i=0; i<size; i++) {
+                // stupid input verification
+                String[] stringInput;
+                do {
+                    stringInput = JOptionPane.showInputDialog(shizune, "Enter Row #" + (i + 1) + ", with " + size + " elements, split by commas.").split(",");
+                    if (stringInput.length != size) {
+                        JOptionPane.showMessageDialog(shizune, "Invalid size! " + stringInput.length + " elements entered but " + size + " required.");
+                    }
+                } while(stringInput.length != size);
+                
+            }
         
             // test toString()
-            JOptionPane.showMessageDialog(shizune, ourBelovedUser);
+            JOptionPane.showMessageDialog(shizune, matrix);
         } while (JOptionPane.showConfirmDialog(shizune, "Do you wish to exit?", "Exit", JOptionPane.YES_NO_OPTION) == 1);
     }
 }
