62719367ef49b8ce4a8cc4c8e63f1b42bc266597
PatheticMustan <patheticmustan@gmail.com>
2020-06-08 23:45:06 -0400

    more

--- Final/HorseRacing.java
+++ Final/HorseRacing.java
@@ -1,43 +1,80 @@
+/** HorseRacing.java            by Kevin Wang
+ * 
+ * A game about racing horses. Spam [Space] as quickly as you can, and get to the end before everybody else!
+ * 
+ **/
+
 import java.awt.*;
 import java.awt.image.*;
+import java.awt.event.*;
 
-import java.io.*;
+import java.util.*;
+
+import java.util.Timer;
 
 import javax.swing.*;
 
+import java.io.*;
+import javax.imageio.ImageIO;
+
 public class HorseRacing extends JPanel {
-    private int width = 512;
-    private int height = 512;
-    private int modifier = 0;
+    private int width;
+    private int height;
+    private Timer timer;
+    private boolean gameOver;
+    private Random random;
+    private BufferedImage horseImage;
     
-    public void update(Graphics g) {
-        paint(g);
-        modifier += 1;
-    }   
+    private int[] distances;
+    
+    public void startGame() {
+        width = 512;
+        height = 512;
+        gameOver = false;
+        timer = new Timer();
+        random = new Random();
+        
+        // import horse image
+        try {
+            horseImage = ImageIO.read(new File("horse.png"));
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+        
+        // runs repaint function 60 fps
+        // thank you https://www.javatpoint.com/post/java-timer-schedule-method
+        TimerTask task = new TimerTask() {
+            public void run() {
+                if (gameOver == false) repaint();
+            }
+        };  
+        
+        timer.schedule(task, 0, 1000 / 60);
+    }
     
     public void paint(Graphics g) {
-        BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
-        Graphics2D g2d = image.createGraphics();
+        super.paint(g);
         
         // white background
-        g2d.setColor(new Color(255, 255, 255));
-        g2d.fillRect(0, 0, width, height);
+        g.setColor(new Color(255, 255, 255));
+        g.fillRect(0, 0, width, height);
         
-        g2d.setColor(new Color(0, 0, 0));
-        g2d.setFont(new Font("Serif", Font.BOLD, 20));
-        g2d.drawString("Test", 30, 30);
+        g.setColor(new Color(0, 0, 0));
+        g.setFont(new Font("Serif", Font.BOLD, 20));
+        g.drawString("Horse Racing!", 30, 30);
         
-        g2d.dispose();
+        g.setFont(new Font("Serif", Font.BOLD, 20));
+        g.drawString("Spam [Space] as fast as you can!", 30, 60);
         
-
-        // display it on the frame
-        g.drawImage(image, 0, 0, null);
+        g.drawImage(horseImage, 30, 100, null);
     }
 
     public static void main(String[] args) {
         HorseRacing hr = new HorseRacing();
         JFrame frame = new JFrame();
         frame.getContentPane().add(hr);
+        
+        hr.startGame();
 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(hr.width, hr.height);
