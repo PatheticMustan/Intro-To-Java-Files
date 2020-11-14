21bafba5cd18e322c2a5c27ce3f1ca81887b69af
Mustan Pathetic <patheticmustan@gmail.com>
2020-11-14 18:23:27 -0500

    Update Lesson7.java

--- Q1/Lessons/Lesson7/Lesson7.java
+++ Q1/Lessons/Lesson7/Lesson7.java
@@ -1,31 +1,17 @@
-/*
- * Lesson7.java
- * Kevin Wang
- * 
- * Prints all the types of variables.
- */
-
 public class Lesson7 {
-   /*
-    * Prints all the types of variables.
-    */
+   // Prints all the types of variables.
    public static void main (String[] args) {
        // initialize vars
       int meaningOfLife = 0;
       double smallPie = 0.0;
       char respectKey = 'A';
-      boolean isWorldOver = true;
+      boolean isWorldOver = false;
       
-      System.out.println("int: " + meaningOfLife);
-      System.out.println("double: " + smallPie);
-      System.out.println("char: " + respectKey);
-      System.out.println("bool: " + isWorldOver);
-       
       // set values
       meaningOfLife = 42;
       smallPie = 3.1415;
       respectKey = 'F';
-      isWorldOver = false;
+      isWorldOver = true;
       
       System.out.println("int: " + meaningOfLife);
       System.out.println("double: " + smallPie);
