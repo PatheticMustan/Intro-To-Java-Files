6481d65910bdf867a62bf825b456bff0f69f8c58
Mustan Pathetic <patheticmustan@gmail.com>
2020-11-15 01:18:45 -0500

    Update TenToFifty.java

--- Q1/Lessons/Lesson14/TenToFifty.java
+++ Q1/Lessons/Lesson14/TenToFifty.java
@@ -1,20 +1,16 @@
-/* TenToFifty.java            Kevin Wang
- * 
- * Write a program segment to display all integers between 10 and 50 (inclusive).
- */
-
 public class TenToFifty {
-    public static void main(String[] args) {
-        int tens = 1;
-        int ones = 0;
-        
-        while(tens <= 5) {
-            ones = 0;
-            while (ones <= 9 && ((tens*10)+ones)<=50) {
-                System.out.println("" + tens + ones++);
-            }
-            tens++;
-        }
-        
-    }
+	public static void main(String[] args) {
+		int tens = 1;
+		int ones = 0;
+		
+		while((tens*10)+ones <= 50) {
+			System.out.println("" + tens + ones);
+			ones++;
+
+			if (ones == 10) {
+				ones = 0;
+				tens++;
+			}
+		}
+	}
 }
